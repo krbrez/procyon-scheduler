1f2f19b101cb43abfd9906e96f2c9511eae3617d
krbrez <38116068+krbrez@users.noreply.github.com>
2018-11-28 13:20:32 -0500

    Write CreateScheduleRequest

--- src/main/java/procyonScheduler/scheduler/CreateScheduleRequest.java
+++ src/main/java/procyonScheduler/scheduler/CreateScheduleRequest.java
@@ -2,14 +2,22 @@ package procyonScheduler.scheduler;
 
 public class CreateScheduleRequest {
 	String name;
-	double value;
-	
-	public CreateScheduleRequest (String n, double v) {
-		this.name = n;
-		this.value = v;
+	String startT;
+	String startD;
+	String endT;
+	String endD;
+	int blockSize;
+
+	public CreateScheduleRequest(String name, String startT, String startD, String endT, String endD, int blockSize) {
+		this.name = name;
+		this.startT = startT;
+		this.startD = startD;
+		this.endT = endT;
+		this.endD = endD;
+		this.blockSize = blockSize;
 	}
-	
+
 	public String toString() {
-		return "Create(" + name + "," + value + ")";
+		return "Create(" + name + "," + startT + "," + startD + "," + endT + "," + endD + "," + blockSize + ")";
 	}
 }
