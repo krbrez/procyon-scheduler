1e73294b675207caef2d696663426728ac6e141d
krbrez <38116068+krbrez@users.noreply.github.com>
2018-11-27 22:27:23 -0500

    Creating Lambda function

--- src/main/java/procyonScheduler/scheduler/CreateScheduleHandler.java
+++ src/main/java/procyonScheduler/scheduler/CreateScheduleHandler.java
@@ -1,42 +1,122 @@
 package procyonScheduler.scheduler;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
+import com.google.gson.Gson;
+
+import edu.wpi.cs.heineman.db.ConstantsDAO;
+import edu.wpi.cs.heineman.demo.CreateConstantRequest;
+import edu.wpi.cs.heineman.demo.CreateConstantResponse;
+import edu.wpi.cs.heineman.model.Constant;
 
 public class CreateScheduleHandler implements RequestHandler<S3Event, String> {
 
-    private AmazonS3 s3 = AmazonS3ClientBuilder.standard().build();
+	public LambdaLogger logger = null;
 
-    public CreateScheduleHandler() {}
+	/** Load from RDS, if it exists
+	 * 
+	 * @throws Exception 
+	 */
+	boolean createConstant(String name, double value) throws Exception {
+		if (logger != null) { logger.log("in createConstant"); }
+		ConstantsDAO dao = new ConstantsDAO();
+		
+		// check if present
+		Constant exist = dao.getConstant(name);
+		Constant constant = new Constant (name, value);
+		if (exist == null) {
+			return dao.addConstant(constant);
+		} else {
+			return dao.updateConstant(constant);
+		}
+	}
+	
+	@Override
+	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
+		logger = context.getLogger();
+		logger.log("Loading Java Lambda handler to create constant");
 
-    // Test purpose only.
-    CreateScheduleHandler(AmazonS3 s3) {
-        this.s3 = s3;
-    }
+		JSONObject headerJson = new JSONObject();
+		headerJson.put("Content-Type",  "application/json");  // not sure if needed anymore?
+		headerJson.put("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+	    headerJson.put("Access-Control-Allow-Origin",  "*");
+	        
+		JSONObject responseJson = new JSONObject();
+		responseJson.put("headers", headerJson);
 
-    @Override
-    public String handleRequest(S3Event event, Context context) {
-        context.getLogger().log("Received event: " + event);
+		CreateConstantResponse response = null;
+		
+		// extract body from incoming HTTP POST request. If any error, then return 422 error
+		String body;
+		boolean processed = false;
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+			JSONParser parser = new JSONParser();
+			JSONObject event = (JSONObject) parser.parse(reader);
+			logger.log("event:" + event.toJSONString());
+			
+			String method = (String) event.get("httpMethod");
+			if (method != null && method.equalsIgnoreCase("OPTIONS")) {
+				logger.log("Options request");
+				response = new CreateConstantResponse("name", 200);  // OPTIONS needs a 200 response
+		        responseJson.put("body", new Gson().toJson(response));
+		        processed = true;
+		        body = null;
+			} else {
+				body = (String)event.get("body");
+				if (body == null) {
+					body = event.toJSONString();  // this is only here to make testing easier
+				}
+			}
+		} catch (ParseException pe) {
+			logger.log(pe.toString());
+			response = new CreateConstantResponse("Bad Request:" + pe.getMessage(), 422);  // unable to process input
+	        responseJson.put("body", new Gson().toJson(response));
+	        processed = true;
+	        body = null;
+		}
 
-        // Get the object from the event and show its content type
-        String bucket = event.getRecords().get(0).getS3().getBucket().getName();
-        String key = event.getRecords().get(0).getS3().getObject().getKey();
-        try {
-            S3Object response = s3.getObject(new GetObjectRequest(bucket, key));
-            String contentType = response.getObjectMetadata().getContentType();
-            context.getLogger().log("CONTENT TYPE: " + contentType);
-            return contentType;
-        } catch (Exception e) {
-            e.printStackTrace();
-            context.getLogger().log(String.format(
-                "Error getting object %s from bucket %s. Make sure they exist and"
-                + " your bucket is in the same region as this function.", key, bucket));
-            throw e;
-        }
-    }
+		if (!processed) {
+			CreateConstantRequest req = new Gson().fromJson(body, CreateConstantRequest.class);
+			logger.log(req.toString());
+
+			CreateConstantResponse resp;
+			try {
+				if (createConstant(req.name, req.value)) {
+					resp = new CreateConstantResponse("Successfully defined constant:" + req.name);
+				} else {
+					resp = new CreateConstantResponse("Unable to create constant: " + req.name, 422);
+				}
+			} catch (Exception e) {
+				resp = new CreateConstantResponse("Unable to create constant: " + req.name + "(" + e.getMessage() + ")", 403);
+			}
+
+			// compute proper response
+	        responseJson.put("body", new Gson().toJson(resp));  
+		}
+		
+        logger.log("end result:" + responseJson.toJSONString());
+        logger.log(responseJson.toJSONString());
+        OutputStreamWriter writer = new OutputStreamWriter(output, "UTF-8");
+        writer.write(responseJson.toJSONString());  
+        writer.close();
+	}
+}
 }
