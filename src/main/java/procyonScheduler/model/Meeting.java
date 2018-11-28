6187ce3e66f7253dde5ad40a562fca560b59829b
krbrez <38116068+krbrez@users.noreply.github.com>
2018-11-28 12:43:53 -0500

    Finshed SchedulesDAO

--- src/main/java/procyonScheduler/model/Meeting.java
+++ src/main/java/procyonScheduler/model/Meeting.java
@@ -3,10 +3,9 @@ package procyonScheduler.model;
 import java.util.GregorianCalendar;
 import java.util.Random;
 
-public class Meeting { 
+public class Meeting {
 	String label; // the name or email address provided by a participant when
 					// they book a meeting
-	String id;
 	GregorianCalendar dateTime; // the date and time of the meeting
 	boolean available; // is the meeting currently booked or not
 	Schedule schedule; // all meetings are within a schedule
@@ -14,10 +13,9 @@ public class Meeting {
 									// meeting, also functions as meeting id
 
 	// constructor for getting an existing meeting
-	public Meeting(String label, String id, GregorianCalendar dateTime, boolean available, Schedule schedule,
+	public Meeting(String label, GregorianCalendar dateTime, boolean available, Schedule schedule,
 			String participantSecretCode) {
 		this.label = label;
-		this.id = id;
 		this.dateTime = dateTime;
 		this.available = available;
 		this.schedule = schedule;
@@ -42,45 +40,26 @@ public class Meeting {
 				this.participantSecretCode += (char) (n + 60);
 			}
 		}
-		this.id = "";
-		Random m = new Random();
-		for (int i = 0; i < 16; i++) {
-			int n = m.nextInt(62);
-			if (n < 10) {
-				this.id += Integer.toString(n);
-			} else if (n <36) {
-				this.id += (char) (n + 55);
-			} else {
-				this.id += (char) (n + 60);
-			}
-		}
 	}
 
-	// getters and setters
-	public boolean getAvailable() {
-		return available;
-	}
-	
+	// relevant setters
 	public void setAvailable() {
 		this.available = true;
+	}
+
+	// relevant getters
+	public boolean getAvailable() {
+		return available;
 	}
 
 	public String getParticipantSecretCode() {
 		return participantSecretCode;
 	}
-	
+
 	public void setParticipantSecretCode(String participantSecretCode) {
 		this.participantSecretCode = participantSecretCode;
 	}
 
-	public String getId() {
-		return id;
-	}
-	
-	public void setId(String id) {
-		this.id = id;
-	}
-	
 	public String getLabel() {
 		return label;
 	}
