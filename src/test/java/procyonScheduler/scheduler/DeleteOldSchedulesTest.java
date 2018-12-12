cc6861d0b736a62bf6cf410f33e6f4ad389e3484
krbrez <38116068+krbrez@users.noreply.github.com>
2018-12-12 18:35:38 -0500

    Finished test case for DeleteOldSchedules

---
+++ src/test/java/procyonScheduler/scheduler/DeleteOldSchedulesTest.java
@@ -0,0 +1,63 @@
+package procyonScheduler.scheduler;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.GregorianCalendar;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.google.gson.Gson;
+
+import procyonScheduler.db.SchedulesDAO;
+import procyonScheduler.model.Schedule;
+
+/**
+ * A simple test harness for locally invoking your Lambda function handler.
+ */
+public class DeleteOldSchedulesTest {
+
+	Context createContext(String apiCall) {
+		TestContext ctx = new TestContext();
+		ctx.setFunctionName(apiCall);
+		return ctx;
+	}
+
+	// Play with meeting 'hiQ3vL2P8cxn0JnQ'
+	// Available (open) is 1, unavailable (closed) = 0
+
+	@Test
+	public void testDeleteOldSchedules() throws Exception {
+		// Create super old schedule
+		SchedulesDAO sDAO = new SchedulesDAO();
+		GregorianCalendar start = new GregorianCalendar(2018, 11, 28, 10, 00);
+		GregorianCalendar end = new GregorianCalendar(2019, 0, 2, 20, 00);
+		GregorianCalendar created = new GregorianCalendar(2017, 1, 2, 15, 22);
+		Schedule newSched = new Schedule("Kyra", start, end, 15, "2222222222222222", created, "2222222222222222");
+		sDAO.addSchedule(newSched);
+		
+		// Delete super old schedule
+		DeleteOldSchedulesHandler delHandler = new DeleteOldSchedulesHandler();
+
+		DeleteOldSchedulesRequest dosr = new DeleteOldSchedulesRequest(365);
+
+		String delRequest = new Gson().toJson(dosr);
+		String jsonRequest = new Gson().toJson(new HttpRequest(delRequest));
+
+		InputStream input = new ByteArrayInputStream(jsonRequest.getBytes());
+		OutputStream output = new ByteArrayOutputStream();
+
+		delHandler.handleRequest(input, output, createContext("delete old schedules"));
+
+		HttpResponse del = new Gson().fromJson(output.toString(), HttpResponse.class);
+		DeleteOldSchedulesResponse resp = new Gson().fromJson(del.body, DeleteOldSchedulesResponse.class);
+
+		Assert.assertTrue(resp.response.contains("1 schedules"));
+		Assert.assertEquals(resp.httpCode, 200);
+
+	}
+
+}
