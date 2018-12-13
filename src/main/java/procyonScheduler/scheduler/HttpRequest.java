b6ed11dc36dd3a759fb446074adfe1379ecebd79
krbrez <38116068+krbrez@users.noreply.github.com>
2018-12-13 17:49:42 -0500

    Updated commenting for HTTP Request/Response

--- src/main/java/procyonScheduler/scheduler/HttpRequest.java
+++ src/main/java/procyonScheduler/scheduler/HttpRequest.java
@@ -1,9 +1,16 @@
 package procyonScheduler.scheduler;
 
-
+/** 
+ * A class for generic HTTP requests, mostly for testing
+ *
+ */
 public class HttpRequest {
-	String body;
+	String body; // The body of the request
 	
+	/**
+	 * Constructor
+	 * @param s The body of the request
+	 */
 	public HttpRequest(String s) {
 		body = s;
 	}
--- src/main/java/procyonScheduler/scheduler/HttpResponse.java
+++ src/main/java/procyonScheduler/scheduler/HttpResponse.java
@@ -1,8 +1,18 @@
 package procyonScheduler.scheduler;
 
+/**
+ * A class for generic HTTP responses, mostly for testing
+ *
+ */
 public class HttpResponse {
-	String body;
-	
+	String body; // The body of the response
+
+	/**
+	 * Constructor
+	 * 
+	 * @param b
+	 *            The body of the response
+	 */
 	public HttpResponse(String b) {
 		body = b;
 	}
